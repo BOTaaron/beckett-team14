82454de8bde120306a76f656227db832f86db234
Ashley H <ashleyhughes223@gmail.com>
2022-03-10 11:00:48 +0000

    Updated database functions (now working)

---
+++ Database/createAccount.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="main.css"/>
+        <title>Results</title>
+</head>
+<body>
+    <h1>Insert a record</h1>
+
+    <form method="post" action="insertRecord.php">
+
+        <input type="text" name="txtID" />
+        <br />
+        <input type="text" name="txtName" />
+        <br />
+        <input type="text" name="txtPass" />
+        <br />
+        <input type="submit" value="Submit" name="subUser" />   
+    </form>
+    <?php
+
+      //  include 'select.php';
+
+    ?>
+
+    </body>
+</html>
+
+
+
